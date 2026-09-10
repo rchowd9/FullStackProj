177a2d671933fcc12af423e7dfdc9664a6b7af7f
rchowd9 <chowdhuryriasat078@gmail.com>
2026-09-09 20:18:16 -0400

    fixed capitalization

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ const missions = [
     category: 'Distributed Systems',
     difficulty: 'Advanced',
     xp: 580,
-    description: 'collection of independent computers and devices that work together over a network.', 
+    description: 'Collection of independent computers and devices that work together over a network.', 
   }, 
 
   {
